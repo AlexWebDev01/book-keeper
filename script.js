1e8e0d2063e439a17850af83286722c0301632ca
Alex Udodov <102874212+AlexWebDev01@users.noreply.github.com>
2022-07-03 18:25:21 +0300

    code review

--- script.js
+++ script.js
@@ -6,13 +6,12 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
-let bookmarks = [];
-
+let bookmarks = {};
 
 // Show Modal, Focus on Input
 function showModal() {
-    modal.classList.add('show-modal');
-    websiteNameEl.focus();
+	modal.classList.add('show-modal');
+	websiteNameEl.focus();
 }
 
 // Modal Event Listeners
@@ -22,105 +21,107 @@ window.addEventListener('click', (e) => (e.target === modal ? modal.classList.re
 
 // Validate Form
 function validate(nameValue, urlValue) {
-    const exression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
-    const regex = new RegExp(exression);
-    if (!nameValue || !urlValue) {
-        alert('Please submit valued for both fields');
-        return false;
-    }
-    if (!urlValue.match(regex)) {
-        alert('Please provide a valid web address');
-        return false;
-    }
-    // Valid
-    return true;
+	const expression = /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
+	const regex = new RegExp(expression);
+	if (!nameValue || !urlValue) {
+		alert('Please submit values for both fields.');
+		return false;
+	}
+	if (!urlValue.match(regex)) {
+		alert('Please provide a valid web address.');
+		return false;
+	}
+	// Valid
+	return true;
+}
+
+// Build Bookmarks
+function buildBookmarks() {
+	// Remove all bookmark elements
+	bookmarksContainer.textContent = '';
+	// Build items
+	Object.keys(bookmarks).forEach((id) => {
+
+		const { name, url } = bookmarks[id];
+
+		// Item
+		const item = document.createElement('div');
+		item.classList.add('item');
+		// Close Icon
+		const closeIcon = document.createElement('i');
+		closeIcon.classList.add('fas', 'fa-times');
+		closeIcon.setAttribute('title', 'Delete Bookmark');
+		closeIcon.setAttribute('onclick', `deleteBookmark('${id}')`);
+		// Favicon / Link Container
+		const linkInfo = document.createElement('div');
+		linkInfo.classList.add('name');
+		// Favicon
+		const favicon = document.createElement('img');
+		favicon.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
+		favicon.setAttribute('alt', 'Favicon');
+		// Link
+		const link = document.createElement('a');
+		link.setAttribute('href', `${url}`);
+		link.setAttribute('target', '_blank');
+		link.textContent = name;
+		// Append to bookmarks container
+		linkInfo.append(favicon, link);
+		item.append(closeIcon, linkInfo);
+		bookmarksContainer.appendChild(item);
+	});
+}
+
+// Fetch bookmarks
+function fetchBookmarks() {
+	// Get bookmarks from localStorage if available
+	if (localStorage.getItem('bookmarks')) {
+		bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+	} else {
+		// Create bookmarks object in localStorage
+		const id = `https://github.com/AlexWebDev01`
+		bookmarks[id] = {
+			name: 'Alex Development',
+			url: 'https://github.com/AlexWebDev01',
+		}
+
+		localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+	}
+	buildBookmarks();
 }
 
 // Delete Bookmark
-function deleteBookmark(url) {
-    bookmarks.forEach((bookmark, i) => {
-        if (bookmark.url === url) {
-            bookmarks.splice(i, 1);
-        }
-    });
-    //  Update bookmarks array in localStorage, re-populate DOM
-    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-    fetchBookmarks();
+function deleteBookmark(id) {
+	// Loop through the bookmarks array
+	if (bookmarks[id]) {
+		delete bookmarks[id]
+	}
+	// Update bookmarks array in localStorage, re-populate DOM
+	localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+	fetchBookmarks();
 }
 
-// Build Bookmarks DOM
-function buildBookmarks() {
-    // Remove all bookmark elements
-    bookmarksContainer.textContent = '';
-    // Build items
-    bookmarks.forEach((bookmark) => {
-        const { name, url } = bookmark;
-        //  Item
-        const item = document.createElement('div');
-        item.classList.add('item');
-        //  Close Icon
-        const closeIcon = document.createElement('i');
-        closeIcon.classList.add('fas', 'fa-times');
-        closeIcon.setAttribute('title', 'Delete Bookmarks');
-        closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
-        // Favicon / Link Container
-        const linkInfo = document.createElement('div');
-        linkInfo.classList.add('name');
-        // Favicon
-        const favicon = document.createElement('img');
-        favicon.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
-        favicon.setAttribute('alt', 'Favicon');
-        // Link
-        const link = document.createElement('a');
-        link.setAttribute('href', `${url}`);
-        link.setAttribute('target', '_blank');
-        link.textContent = name;
-        // Append to bookmarks container
-        linkInfo.append(favicon, link);
-        item.append(closeIcon, link);
-        bookmarksContainer.appendChild(item);
-    });
-}
-
-// Fetch Bookmarks
-function fetchBookmarks() {
-    // Get bookmarks from localStorage if available
-    if (localStorage.getItem('bookmarks')) {
-        bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-    } else {
-        // Create bookmarks array in localStorage
-        bookmarks = [
-            {
-                name: 'Alex Development',
-                url: 'https://github.com/AlexWebDev01'
-            },
-        ];
-        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-    }
-    buildBookmarks();
-}
-
-// Handle data from Form
 function storeBookmark(e) {
-    e.preventDefault();
-    const nameValue = websiteNameEl.value;
-    let urlValue = websiteUrlEl.value;
-    if (!urlValue.includes('http://') && !urlValue.includes("https://")) {
-        urlValue = `https://${urlValue}`;
-    }
-    
-    if (!validate(nameValue, urlValue)) {
-        return false;
-    }
-    const bookmark = {
-        name: nameValue,
-        url: urlValue,
-    };
-    bookmarks.push(bookmark);
-    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-    fetchBookmarks();
-    bookmarkForm.reset();
-    websiteNameEl.focus();
+	e.preventDefault();
+	const nameValue = websiteNameEl.value;
+	let urlValue = websiteUrlEl.value;
+	if (!urlValue.includes('http://', 'https://')) {
+		urlValue = `https://${urlValue}`;
+	}
+	// Validate
+	if (!validate(nameValue, urlValue)) {
+		return false;
+	}
+	// Set bookmark object, add to array
+	const bookmark = {
+		name: nameValue,
+		url: urlValue,
+	};
+	bookmarks[urlValue] = bookmark;
+	// Set bookmarks in localStorage, fetch, reset input fields
+	localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+	fetchBookmarks();
+	bookmarkForm.reset();
+	websiteNameEl.focus();
 }
 
 // Event Listener
